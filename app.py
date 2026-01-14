664b4d7c73e6a50e37fe96ad5154236d534c4344
Arwan Muhtada <147142021+wanmuhtd@users.noreply.github.com>
2026-01-15 03:29:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,20 @@ except Exception as e:
 # ======================================================
 # 4. MAIN LAYOUT
 # ======================================================
-st.markdown("<h1 style='text-align: center; color: white;'>Diabetes Intelligence System</h1>", unsafe_allow_html=True)
+st.markdown(
+    """
+    <div style="
+        text-align: center;
+        color: white;
+        font-size: 2.5rem;
+        font-weight: 700;
+        margin-bottom: 1rem;
+    ">
+        Prediksi Risiko Diabetes
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 st.markdown("<p style='text-align: center; color: #8b949e; margin-bottom: 30px;'>Kompensasi Fitur Denoising Autoencoder & Klasifikasi Stacking Ensemble</p>", unsafe_allow_html=True)
 
 tab1, tab2 = st.tabs(["🔍 Prediction Tool", "📖 Methodology"])
@@ -232,6 +245,7 @@ with tab2:
         2. **Denoising Autoencoder (DAE)**: Digunakan untuk mengkompensasi fitur (menghilangkan noise) khusus pada pasien dewasa (> 30 tahun).
         3. **Stacking Ensemble**: Menggabungkan beberapa model klasifikasi dasar untuk meningkatkan akurasi akhir prediksi risiko diabetes.
         """)
+
 
 
 
